fd0b7789c9b397c855e749824b409812129c4e17
Sammy-Jo Wymer <sammy-jo.wymer@diconium.com>
2022-11-03 11:50:10 +0100

    virtual scrolling code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
 function App() {
+  const numbers = Array.from(Array(9000).keys());
+  // const manyNumbers = Array.from(Array(100000).keys());
+  const windowHeight = 600;
+
+  const limit = 20; // amount of trs
+
+  const [scrollTop, setScrollTop] = useState(0);
+
+  const rowHeight = 55;
+  const scrolledPast = Math.floor(scrollTop / rowHeight); // calculates how many rows we've scrolled past
+  const startRowHeight = scrolledPast * rowHeight; // the height of everything you've scrolled past
+  const endRowHeight =
+    numbers.length * rowHeight - windowHeight - startRowHeight; //
+
+  console.log("scroll top", scrollTop);
+  console.log("scrolled past", scrolledPast);
+  console.log("start height", startRowHeight);
+  console.log("end height", endRowHeight);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Virtual Scrolling</h1>
+      <h3>
+        You've scrolled past {scrollTop}px or {scrolledPast} rows!
+      </h3>
+      <div
+        onScroll={(e) => setScrollTop(e.currentTarget.scrollTop)}
+        style={{ height: windowHeight, overflow: "auto" }}
+      >
+        <table>
+          <tbody>
+            <tr style={{ height: startRowHeight }} />
+            {numbers
+              .slice(scrolledPast, scrolledPast + limit)
+              .map((numbers) => (
+                <tr key={numbers} style={{ height: rowHeight }}>
+                  <td>{numbers}</td>
+                </tr>
+              ))}
+            <tr style={{ height: endRowHeight }} />
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
